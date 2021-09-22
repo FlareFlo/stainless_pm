841b351d6125ad8c4e58e101efd435de99ab060e
FlareFlo <florianlipp17@gmail.com>
2021-09-22 22:11:21 +0200

    revert
    Took 10 minutes

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,10 @@ use crate::crypto::{decrypt, encrypt};
 use std::fs;
 use std::convert::TryInto;
 use std::fs::File;
-use flate2::read::GzEncoder;
-use flate2::Compression;
 
 mod crypto;
 
 
 fn main() {
-	let tar_archive = File::create("password.tar.gz").unwrap();
-	let enc = GzEncoder::new(tar_archive, Compression::default());
-	let mut tar = tar::Builder::new(enc);
-	tar.append_dir_all("./cum2", "./cum").unwrap();
+
 }
