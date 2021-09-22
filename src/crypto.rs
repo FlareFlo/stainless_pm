0f92edf28e715ef7101dd852c93fac993ee0b10b
FlareFlo <florianlipp17@gmail.com>
2021-09-22 20:08:06 +0200

    Preparing tar archive for storing encrypted data
    Took 50 minutes

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,19 +1,13 @@
-use aes_gcm::{Aes256Gcm, Key, Nonce};
-use aes_gcm::aead::{Aead, NewAead, AeadMut};
-use argon2::{Argon2, PasswordHasher};
-use argon2::password_hash::SaltString;
-use rand::rngs::OsRng;
-use rand::Rng;
-use aes_gcm::aes::cipher::generic_array::sequence::GenericSequence;
-use aes_gcm::aead::generic_array::GenericArray;
-use aes_gcm::AeadCore;
 use std::convert::TryFrom;
 
-pub(crate) struct Entry {
-	openname: String,
-	closedname: Option<String>,
-}
+use aes_gcm::{Aes256Gcm, Key, Nonce};
+use aes_gcm::aead::{Aead, NewAead};
+use argon2::{Argon2, PasswordHasher};
+use argon2::password_hash::SaltString;
+use rand::Rng;
+use rand::rngs::OsRng;
 
+#[derive(Clone, Hash, Debug, Eq, Ord, PartialOrd, PartialEq)]
 pub struct EncryptedReturn {
 	pub ciphertext: Vec<u8>,
 	pub salt: Vec<u8>,
@@ -33,16 +27,16 @@ pub fn encrypt(value: Vec<u8>, password: &str) -> EncryptedReturn {
 	let encryptedreturn = EncryptedReturn {
 		ciphertext: cipher.encrypt(nonce, value.as_slice()).unwrap(),
 		salt: Vec::from(salt.as_bytes()),
-		nonce: <[u8; 12]>::try_from(nonce.as_slice()).unwrap()
+		nonce: <[u8; 12]>::try_from(nonce.as_slice()).unwrap(),
 	};
-	return encryptedreturn
+	return encryptedreturn;
 }
 
-pub fn decrypt(encryptedreturn: EncryptedReturn, password:&str) -> Vec<u8>{
+pub fn decrypt(encryptedreturn: EncryptedReturn, password: &str) -> Vec<u8> {
 	let nonce = Nonce::from_slice(&encryptedreturn.nonce);
 	let password_hash = Argon2::default().hash_password(password.as_bytes(), &String::from_utf8(encryptedreturn.salt).unwrap()).unwrap().hash.unwrap();
 	let cipher = Aes256Gcm::new(Key::from_slice(password_hash.as_bytes()));
 	let ciphertext = encryptedreturn.ciphertext;
 	let decrypted = cipher.decrypt(nonce, ciphertext.as_slice()).unwrap();
-	return decrypted
+	return decrypted;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use crate::crypto::{EncryptedReturn, encrypt, decrypt};
-use aes_gcm::{Key, Nonce};
-use aes_gcm::Aes256Gcm;
-use aes_gcm::NewAead;
+use crate::crypto::{decrypt, encrypt};
+use std::fs;
+use std::convert::TryInto;
 
 mod crypto;
 
 
 fn main() {
-	let encrypted = encrypt(Vec::from("cum"), "cheese");
 
-	let decrypted = decrypt(encrypted, "cheese");
-	println!("{}", String::from_utf8(decrypted).unwrap());
 }
