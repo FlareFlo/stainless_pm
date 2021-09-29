6ba09c63894526ee9bc37fb438c3f5e3916bf7aa
flareflo <florianlipp17@gmail.com>
2021-09-29 12:57:12 +0200

    Clippy fixes and adaptation to crate changes
    Took 11 minutes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::fs;
 use std::time::Instant;
-use slpm_file::header_v0::{HeaderBinaryV0, DataType, HeaderV0};
+use slpm_file::header_v0::HeaderV0;
+use slpm_file::header_binary_v0::HeaderBinaryV0;
 use slpm_file::payload::Entry;
-// use slpm_file::header_v0::{DataType, HeaderBinaryV0, HeaderV0};
-// use slpm_file::payload::Entry;
-
+use slpm_file::datatype::DataType;
 
 fn main() {
 	encrypt_decrypt_regular();
@@ -12,7 +11,7 @@ fn main() {
 
 fn encrypt_decrypt_regular() {
 	let start = Instant::now();
-	let header = HeaderBinaryV0::from_parameters(&DataType::File, "Warthunder", None, "new", 100000);
+	let header = HeaderBinaryV0::from_parameters(&DataType::File, "Warthunder", None, "new", 100_000);
 	let data = fs::read("./src/assets/War Thunder 2021.09.27 - 14.44.20.02.DVR_Trim.mp4").unwrap(); //local, use any
 	let entry = Entry::encrypt(&data, &header, "cum");
 	fs::write("./src/assets/file.slpm", entry.to_bytes()).unwrap();
@@ -23,7 +22,7 @@ fn encrypt_decrypt_regular() {
 	let serialized = Entry::from_bytes(&read);
 	let decrypted = serialized.decrypt("cum");
 	let decrypted_header = HeaderBinaryV0::from_bytes(&decrypted.header);
-	let serialized_header = HeaderV0::from_binary_header(decrypted_header);
+	let serialized_header = HeaderV0::from_binary_header(&decrypted_header);
 	println!("{:?}", serialized_header);
 	fs::write("./src/assets/", &decrypted.text).unwrap();
 	println!("{:?}", start.elapsed());
